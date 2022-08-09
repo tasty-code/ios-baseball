214316fbf0c9fbeec996607d6777280f5988d3ea
Y00ujin <yujingim43@gmail.com>
2022-08-09 15:18:51 +0900

    feat : 임의의 수 생성 함수(createCorrectNumber) 생성

--- BaseBall/BaseBall/main.swift
+++ BaseBall/BaseBall/main.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
-print("Hello, World!")
+private var correctNumber: [Int] = [Int.random(in: 1...9)]
+private var tryCount = 9
 
+createCorrectNumber()
+
+private func createCorrectNumber(){
+    while(correctNumber.count < 3){
+        let randomNumber = Int.random(in: 1...9)
+        
+        if !correctNumber.contains(randomNumber){
+            correctNumber.append(randomNumber)
+        }
+    }
+}
