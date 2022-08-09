45e7e0abc7eb191fb2632012ee363bd11b012719
Y00ujin <yujingim43@gmail.com>
2022-08-09 16:00:27 +0900

    delete : 코드 제거

--- BaseBall/BaseBall/main.swift
+++ BaseBall/BaseBall/main.swift
@@ -7,17 +7,3 @@
 
 import Foundation
 
-private var correctNumber: [Int] = [Int.random(in: 1...9)]
-private var tryCount = 9
-
-createCorrectNumber()
-
-private func createCorrectNumber(){
-    while(correctNumber.count < 3){
-        let randomNumber = Int.random(in: 1...9)
-        
-        if !correctNumber.contains(randomNumber){
-            correctNumber.append(randomNumber)
-        }
-    }
-}
