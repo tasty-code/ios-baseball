415400e310a146a1df496dc71a4b57b1e49a2956
lcy <pgbenny0035@gmail.com>
2022-08-10 01:08:31 +0900

    makeRandomNumber 함수 구현

--- main/main/main.swift
+++ main/main/main.swift
@@ -1,11 +1,20 @@
-//
-//  main.swift
-//  main
-//
-//  Created by Lim Chae Yoon on 2022/08/10.
-//
-
 import Foundation
 
-print("Hello, World!")
+func makeRandomNumber() -> Array<Int> {
+    var arr: Array<Int> = []
+    
+    for _ in 0...2 {
+        var randomNum = Int.random(in: 1...9)
+        while arr.contains(randomNum) == true {
+            randomNum = Int.random(in: 1...9)
+        }
+        arr.append(randomNum)
+    }
+    
+    return arr
+}
 
+let randomNumber = makeRandomNumber()
+var tryCount = 9
+
+print(randomNumber)
