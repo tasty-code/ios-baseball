f098ab0761b8cf4d3cdb58597e11ef9df1b3f3a0
lcy <pgbenny0035@gmail.com>
2022-08-10 01:12:03 +0900

    playGame 함수 구현

--- main/main/main.swift
+++ main/main/main.swift
@@ -14,7 +14,38 @@ func makeRandomNumber() -> Array<Int> {
     return arr
 }
 
+func playGame() {
+    let myNumber = makeRandomNumber()
+    var strike = 0, ball = 0
+    
+    print("임의의 수 : \(myNumber[0]) \(myNumber[1]) \(myNumber[2])")
+    
+    for i in 0...2 {
+        if myNumber[i] == randomNumber[i] {
+            strike += 1
+        } else if randomNumber.contains(myNumber[i]) {
+            ball += 1
+        }
+    }
+    print("\(strike) 스트라이크, \(ball) 볼")
+    
+    tryCount -= 1
+    if tryCount == 0 {
+        print("컴퓨터 승리...!")
+        print("남은 기회 : \(tryCount)")
+        return
+    } else if strike == 3 {
+        print("사용자 승리!")
+        print("남은 기회 : \(tryCount)")
+        return
+    }
+    
+    print("남은 기회 : \(tryCount)")
+    
+    playGame()
+}
+
 let randomNumber = makeRandomNumber()
 var tryCount = 9
 
-print(randomNumber)
+playGame()
