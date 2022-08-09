a0ce51f9ad0738e42d3a6ecfc241cedea670a4a4
Y00ujin <yujingim43@gmail.com>
2022-08-09 16:14:14 +0900

    feat : createRandomNumber() 임의의 수 생성 함수 생성

--- BaseBall/BaseBall/main.swift
+++ BaseBall/BaseBall/main.swift
@@ -7,3 +7,14 @@
 
 import Foundation
 
+private var correctNumber = [Int]()
+private var tryCount = 9
+
+// MARK: - 임의의 수 생성 함수
+private func createRandomNumber() -> [Int]{
+    var randomNumber = [Int.random(in: 1...3), Int.random(in: 4...6), Int.random(in: 7...9)]
+    randomNumber.shuffle()
+
+    return randomNumber
+}
+
