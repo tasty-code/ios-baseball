0534ee48e028256f906c0775c525ee08ce52f589
Y00ujin <yujingim43@gmail.com>
2022-08-10 10:33:18 +0900

    refactor : 마크 주석 제거

--- BaseBall/BaseBall/main.swift
+++ BaseBall/BaseBall/main.swift
@@ -10,7 +10,6 @@ import Foundation
 var correctNumber = [Int]()
 var tryCount = 9
 
-// MARK: - 임의의 수 생성 함수
 func createRandomNumber() -> [Int] {
     var randomNumber = [Int.random(in: 1...3), Int.random(in: 4...6), Int.random(in: 7...9)]
     randomNumber.shuffle()
@@ -18,7 +17,6 @@ func createRandomNumber() -> [Int] {
     return randomNumber
 }
 
-// MARK: - 임의의 수와 입력 수 비교 함수
 func compareCorrectNumber(with number: [Int]) {
     var strikeCount = 0
     var ballCount = 0
@@ -39,7 +37,6 @@ func compareCorrectNumber(with number: [Int]) {
     print("\(strikeCount) 스트라이크, \(ballCount) 볼")
 }
 
-// MARK: - 게임 진행 함수
 func startGame() {
     correctNumber = createRandomNumber()
     
