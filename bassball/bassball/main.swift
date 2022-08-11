a827c7cffe1ac91e502b21fb2bfe94afd006ecb0
parkseyoon <issou06261@gmail.com>
2022-08-11 11:20:51 +0900

    feat : 임의의 정수 생성 함수 구현

--- bassball/bassball/main.swift
+++ bassball/bassball/main.swift
@@ -1,1 +1,17 @@
+import Foundation
 
+
+func makeNumbers() -> Array<Int> {
+
+    var firstNumber = 0
+    var secondNumber = 0
+    var thirdNumber = 0
+    
+    while firstNumber == secondNumber || secondNumber == thirdNumber || firstNumber == thirdNumber {
+        firstNumber = Int.random(in: 1...9)
+        secondNumber = Int.random(in: 1...9)
+        thirdNumber = Int.random(in: 1...9)
+    }
+
+    return [firstNumber, secondNumber, thirdNumber]
+}
