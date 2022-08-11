7bf875a59cddf93b795661fa55ebcdac64ab6440
Schro2109 <bloodparty297@gmail.com>
2022-08-11 11:08:18 +0900

    코드리뷰 피드백 반영

--- CU-iOS Baseball/CU-iOS Baseball/main.swift
+++ CU-iOS Baseball/CU-iOS Baseball/main.swift
@@ -1,76 +1,75 @@
 import Foundation
 
-let maxChance: Int = 9
-gameStart()
+startingGame()
 
-func gameStart() {
-    var chance = maxChance
-    let gameRandomNumbers = getRandomNumbers()
-    
+func startingGame() {
+
+    var chance: Int = 9
+    let gameRandomNumbers: [Int] = getRandomNumbers()
+
     while chance > 0 {
-        chance = chance - 1
-        
+        chance -= 1
+
         let userRandomNumbers: [Int] = getRandomNumbers()
-        print("임의의 수 : \(userRandomNumbers[0]), \(userRandomNumbers[1]), \(userRandomNumbers[2])")
         
-        let roundResult = compare(gameRandomNumbers, and: userRandomNumbers)
+        print("임의의 수 : ",terminator: "")
+        
+        for element in userRandomNumbers {
+            print("\(element) ",terminator: "")
+        }
+        
+        print()
+
+        let roundResult: String = compare(gameRandomNumbers, and: userRandomNumbers)
+        
         if roundResult == "win" {
             print("사용자 승리!")
             return
         }
-        
+
         print("남은 기회 : \(chance)")
     }
-    
-    print("컴퓨터 승리!")
+
+    print("컴퓨터 승리...!")
 }
 
 func getRandomNumbers() -> [Int] {
-    var arr: [Int] = []
-    while arr.count < 3 {
+
+    var randomNumbers: Set<Int> = Set<Int>()
+
+    while randomNumbers.count < 3 {
+
         let randomNum: Int = Int.random(in: 1...9)
-        
-        if arr.contains(randomNum) {
-            
-            continue
-        }
-        
-        arr.append(randomNum)
+
+        randomNumbers.insert(randomNum)
     }
     
-    return arr
+    let randomNumbersToReturn = Array(randomNumbers)
+    
+    return randomNumbersToReturn
 }
 
-func compare(_ gameRandomNumbers: [Int], and: [Int]) -> String{
-    var userRandomNumbers = and
+func compare(_ gameRandomNumbers: [Int], and userNumbers: [Int]) -> String {
     var strikeCount: Int = 0
     var ballCount: Int = 0
     var roundResult: String = ""
     
-    for i in 0...2 {
-        if gameRandomNumbers[i] == userRandomNumbers[i] {
 
-            userRandomNumbers[i] = 0
-            strikeCount = strikeCount + 1
+    for i in 0...2 {
+        if gameRandomNumbers[i] == userNumbers[i] {
+            strikeCount += 1
+        } else if gameRandomNumbers.contains(userNumbers[i]) {
+            ballCount += 1
         }
     }
-    for userNumber in userRandomNumbers {
-        if gameRandomNumbers.contains(userNumber) {
-            
-            ballCount = ballCount + 1
-        }
-    }
-    
+
     print("\(strikeCount) 스트라이크, \(ballCount) 볼")
-    
+
     if strikeCount == 3 {
-        
         roundResult = "win"
-    }else {
-        
+    } else {
         roundResult = "lose"
     }
-    
+
     return roundResult
 }
-
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # ios-baseball
 숫자야구 프로젝트 저장소입니다.
-![FlowChart](./FlowChart/CU-1주차 숫자야구 순서도.png)
+/Users/schro/Desktop/yagom/ios-baseball/FlowChart/CU-1주차 숫자야구 순서도.png
