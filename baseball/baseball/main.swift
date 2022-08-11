28d407a29ac842a81b5f8d7ee8e77344c4ae4851
seunghui5247 <seunghui5247@gmail.com>
2022-08-11 11:05:02 +0900

    refactor: while문 중첩 해결, 전역변수 지역변수 전환

--- baseball/baseball/main.swift
+++ baseball/baseball/main.swift
@@ -6,55 +6,45 @@
 //
 import Foundation
 
-var numbers: [Int] = []
-var userNumbers: [Int] = []
-var randomNumber: Set<Int> = []
 var remainingChance: Int = 9
 
-func makeRandomNumber() {
+func makeRandomNumber() -> [Int] {
+    var randomNumber: Set<Int> = []
+    
     while randomNumber.count < 3 {
         let number = Int.random(in: 1...9)
         randomNumber.insert(number)
     }
-    numbers = Array<Int>(randomNumber)
+    return Array<Int>(randomNumber)
 }
 
-func compareStrikeBall() {
+func compareStrikeBall(userNumber: [Int], randonNumber: [Int]) {
     var strike = 0
     var ball = 0
     
     for i in 0...2 {
-        if numbers[i] == userNumbers[i] {
+        if randonNumber[i] == userNumber[i] {
             strike += 1
-        } else if numbers.contains(userNumbers[i]) {
+        } else if randonNumber.contains(userNumber[i]) {
             ball += 1
         }
     }
     
     remainingChance -= 1
     print("\(strike) 스트라이크 \(ball) 볼")
+    print("남은 기회 : \(remainingChance)")
 }
 
 func startBaseballGame() {
-    makeRandomNumber()
+    let randomNumbers: [Int] = makeRandomNumber()
     
     while remainingChance != 0 {
-        while userNumbers.count < 3 {
-            let number = Int.random(in: 1...9)
-            if userNumbers.contains(number) {
-                continue
-            } else {
-                userNumbers.append(number)
-            }
-        }
+        var userNumbers: [Int] = makeRandomNumber()
         
         print("임의의 수 : ", terminator: " ")
-        for number in userNumbers {
-            print(number, terminator: " ")
-        }
-        print()
+        print(userNumbers.map{ String($0) }.joined(separator: " "))
         
-        compareStrikeBall()
+        compareStrikeBall(userNumber: userNumbers, randonNumber: randomNumbers)
         userNumbers.removeAll()
     }
     remainingChance = 9
