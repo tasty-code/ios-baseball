641f95349396b725e192292eb8a076b05b68106e
lcy <pgbenny0035@gmail.com>
2022-08-12 15:49:58 +0900

    변수명, 함수명 수정

--- main/main/main.swift
+++ main/main/main.swift
@@ -1,24 +1,24 @@
 import Foundation
 
-let randomNumber: Array<Int> = makeRandomNumber()
+let randomNumbers: [Int] = makeRandomNumbers()
 var tryCount: Int = 9
 var strike = 0, ball = 0
 
-func makeRandomNumber() -> Array<Int> {
-    var nonduplicateSet: Set<Int> = []
-    
-    while nonduplicateSet.count < 3 {
-        nonduplicateSet.update(with: Int.random(in: 1...9))
+func makeRandomNumbers() -> [Int] {
+    var nonduplicateNumbers: Set<Int> = []
+
+    while nonduplicateNumbers.count < 3 {
+        nonduplicateNumbers.update(with: Int.random(in: 1...9))
     }
-    
-    return Array(nonduplicateSet)
+
+    return Array(nonduplicateNumbers)
 }
 
-func checkStrikeOrBall(userNumber: Array<Int>) {
+func checkStrikeOrBall(userNumbers: [Int]) {
     for i in 0...2 {
-        if userNumber[i] == randomNumber[i] {
+        if userNumbers[i] == randomNumbers[i] {
             strike += 1
-        } else if randomNumber.contains(userNumber[i]) {
+        } else if randomNumbers.contains(userNumbers[i]) {
             ball += 1
         }
     }
@@ -26,7 +26,7 @@ func checkStrikeOrBall(userNumber: Array<Int>) {
 
 func isGameEnd() -> Bool {
     tryCount -= 1
-    
+
     if strike == 3 {
         print("사용자 승리!")
         print("남은 기회 : \(tryCount)")
@@ -36,26 +36,27 @@ func isGameEnd() -> Bool {
         print("남은 기회 : \(tryCount)")
         return true
     }
-    
+
     return false
 }
 
-func playGame() {
-    let userNumber = makeRandomNumber()
+func startGame() {
+    let userNumbers = makeRandomNumbers()
     strike = 0
     ball = 0
-    
-    print("임의의 수 : \(userNumber[0]) \(userNumber[1]) \(userNumber[2])")
-    checkStrikeOrBall(userNumber: userNumber)
-   
+
+    print("임의의 수 : \(userNumbers[0]) \(userNumbers[1]) \(userNumbers[2])")
+    checkStrikeOrBall(userNumbers: userNumbers)
+
     print("\(strike) 스트라이크, \(ball) 볼")
     if isGameEnd() {
         return
     }
-   
+
     print("남은 기회 : \(tryCount)")
-    
-    playGame()
+
+    startGame()
 }
 
-playGame()
+startGame()
+
