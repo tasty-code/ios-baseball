a17927b293ec33828f4284864b02c9cf40197537
moonsoungjin <moonsungjin@munseongjin-ui-MacBookPro.local>
2022-08-22 08:33:56 +0900

    두 함수를 받아 비교하여 값을 반환하는 함수

--- baseball/baseball/main.swift
+++ baseball/baseball/main.swift
@@ -29,3 +29,33 @@ func userNumber() -> [Int] {
     return [aNumberComparison, bNumberComparison, cNumberComparison]
     
 }
+
+func comparison(computernumber: [Int], usernumber: [Int]) -> [Int] {
+    
+    var strike : Int = 0
+    var ball : Int = 0
+    
+        
+    if  computernumber[0] == usernumber[0]{
+        strike += 1
+    }
+    else if computernumber[0] == usernumber[1] || computernumber[0] == usernumber[2] {
+        ball += 1
+    }
+    
+    if computernumber[1] == usernumber[1] {
+        strike += 1
+    }
+    else if computernumber[1] == usernumber[0] || computernumber[1] == usernumber[2] {
+        ball += 1
+    }
+    
+    if computernumber[2] == usernumber[2] {
+        strike += 1
+    }
+    else if  computernumber[2] == usernumber[0] || computernumber[2] == usernumber[1] {
+        ball += 1
+    }
+    
+    return [strike , ball]
+}
