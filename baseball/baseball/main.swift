06f2207409b4e26cd566b35948287ef29a7ebea4
moonsoungjin <moonsungjin@munseongjin-ui-MacBookPro.local>
2022-08-22 09:05:37 +0900

    컴퓨터 임의의 정수 3개를 만드는 함수

---
+++ baseball/baseball/main.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func computerNumber() -> [Int] {
+    var aNumber : Int = 0
+    var bNumber : Int = 0
+    var cNumber : Int = 0
+        
+    while (aNumber == bNumber || bNumber == cNumber || aNumber == cNumber) {
+        aNumber = Int.random(in: 1...9)
+        bNumber = Int.random(in: 1...9)
+        cNumber = Int.random(in: 1...9)
+    }
+    return [aNumber, bNumber, cNumber]
+}
+
